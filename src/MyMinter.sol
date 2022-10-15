3f01422de8cdf663f84e6c51362a75ed67f3361b
Salief Lewis <salieflewis@gmail.com>
2022-10-15 11:21:47 -0500

    initial commit

---
+++ src/MyMinter.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+// address public ZorbAddress = 0x34fe32e6442d14d923953a537b8163365630b5a7;
+
+error ErrorTransferringFunds();
+error MintingError();
+
+contract MyMinter {
+
+    event MintedFromMyMinter(address target, address to, uint256);
+
+    function purchase(address payable target, uint256 quantity) public {
+        address to = msg.sender;
+
+
+        try IERC721Drop(target).adminMint(to, quantity) {
+            if (msg.value > 0) {
+                // Send value to root contract
+                (bool success, ) = payable(address(target)).call{
+                    value: msg.value
+                }("");
+                if (!success) {
+                    revert.ErrorTransferringFunds();
+                }
+            }
+           emit MintedFromMyMinter(target, to, quantity);
+        } catch {
+            revert MintingError();
+    }
+}
+
