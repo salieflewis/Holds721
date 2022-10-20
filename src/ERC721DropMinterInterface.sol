1f902b2e44cc133a574a5f4925e01468090fd889
Salief Lewis <salieflewis@gmail.com>
2022-10-20 13:46:39 -0400

    fix: cleaned up files but facing compiler issues

--- src/ERC721DropMinterInterface.sol
+++ src/ERC721DropMinterInterface.sol
@@ -2,17 +2,9 @@
 pragma solidity ^0.8.10;
 
 interface ERC721DropMinterInterface {
-    function adminMint(address recipient, uint256 quantity)
-        external
-        returns (uint256);
+    function adminMint(address recipient, uint256 quantity) external returns (uint256);
 
     function hasRole(bytes32, address) external returns (bool);
 
     function isAdmin(address) external returns (bool);
-
-    function setApprovalForAll(address operator, bool approved) external returns (bool);
-
-    function totalSupply() external returns (uint256);
-
-
 }
--- src/ZorbMinter.sol
+++ src/ZorbMinter.sol
@@ -1,20 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.15;
 
 import "openzeppelin-contracts/token/ERC721/ERC721.sol";
 import "zorb/ZorbNFT.sol";
 import "./ERC721DropMinterInterface.sol";
 
-contract ZorbMinter  {
+contract ZorbMinter {
     // ===== ERRORS =====
-
-    // error ZorbsUnapproved();
-    // error NotEnoughZorbs(); dont actually need this -- if not enough zorbs, the mint full price
-
     /// @notice Action is unable to complete because msg.value is incorrect
     error WrongPrice();
-
     error MinterNotAuthorized();
+    error TransferUnsuccessful();
 
     // ===== CONSTANTS =====
     bytes32 public immutable MINTER_ROLE = keccak256("MINTER");
@@ -23,10 +19,6 @@ contract ZorbMinter  {
     // ===== PUBLIC VARIABLES =====
     uint256 public priceInZorbs;
     uint256 public priceInEth;
-    // uint256 public amountOfZorbsHeld;
-    uint256[] public specificZorbsHeld;
-    uint256 public zorbSupply = ZORB_ADDRESS.totalSupply();
-    
 
     // ===== CONSTRUCTOR =====
     constructor(uint256 _priceInZorbs, uint256 _priceInEth) {
@@ -34,62 +26,29 @@ contract ZorbMinter  {
         priceInEth = _priceInEth;
     }
 
-    function mintWithZorbs(
-        address zoraDrop, 
-        address mintRecipient, 
-        uint256 quaantity
-    ) external payable {
-
-        // check if CustomPricingMinter contract has MINTER_ROLE on target ZORA Drop contract
-        if (
-            !ERC721DropMinterInterface(zoraDrop).hasRole(
-                MINTER_ROLE,
-                address(this)
-            )
-        ) {
+    // ===== FUNCTIONS =====
+    function mintWithZorbs(address zoraDrop, address mintRecipient, uint256 quantity) external payable {
+        // Check if ZorbMinter.sol has MINTER_ROLE on target zoraDrop contract
+        if (!ERC721DropMinterInterface(zoraDrop).hasRole(MINTER_ROLE, address(this))) {
             revert MinterNotAuthorized();
         }
 
-        // if msg.sender doesn't have enough Zorbs, they have to pay priceInEth
-        if (priceInZorbs > ZORB_ADDRESS.balanceOf(msg.sender)) {            
-
+        // If msg.sender doesn't have enough Zorbs, they have to pay priceInEth
+        if (priceInZorbs > ZORB_ADDRESS.balanceOf(msg.sender)) {
             if (msg.value != priceInEth) {
                 revert WrongPrice();
             }
 
             ERC721DropMinterInterface(zoraDrop).adminMint(mintRecipient, quantity);
 
-            // Transfer funds to zora drop contract
-            (bool success, ) = zoraDrop.call{value: msg.value}("");
+            // Transfer funds to target zoraDrop contract
+            (bool success,) = zoraDrop.call{value: msg.value}("");
             if (!success) {
-                revert TransferNotSuccessful();
+                revert TransferUnsuccessful();
             }
-
         }
 
-
-        // if msg.sender has enough zorbs, they can mint for free
+        // If msg.sender has enough Zorbs, they can mint for free
         ERC721DropMinterInterface(zoraDrop).adminMint(mintRecipient, quantity);
-
-        // the zorb minter wont actually transfer zorbs out, its just a check
-        //      to see if the person has the zorbs. if they do, they mint for free
-
-        // if (!ZORB_ADDRESS.isApprovedForAll(msg.sender, address(this))) {
-        //     // if user has not approved this minter, approve it
-
-        //     ZORB_ADDRESS.setApprovalForAll(address(this), true);
-        // }
-
-        // for (uint256 index; index < zorbSupply; index++) {
-        //     if (index.ownerOf() == msg.sender) {
-        //         specificZorbsHeld.push(index);
-        //     }
-        // }
-
-        // for (uint256 index; index < priceInZorbs; index++) {
-        //     ZORB_ADDRESS.transferFrom(msg.sender, address(this), specificZorbsHeld[index]);
-        // }
-
-        // Call Admin Mint
     }
 }
