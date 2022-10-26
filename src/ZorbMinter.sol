6db5e5fa8538f8e57b4baa69785ac04e56c9aabf
Salief Lewis <salieflewis@gmail.com>
2022-10-25 20:13:06 -0400

    everything builds but errors still present

--- src/ZorbMinter.sol
+++ src/ZorbMinter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
 
-import "openzeppelin-contracts/token/ERC721/ERC721.sol";
+// import "openzeppelin-contracts/token/ERC721/ERC721.sol";
 import "zorb/ZorbNFT.sol";
 import "./ERC721DropMinterInterface.sol";
 
