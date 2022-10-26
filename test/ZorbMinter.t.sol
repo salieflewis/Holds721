f7aaa4eae399eaf0eaaf4df9e1a624fe38ba049a
Salief Lewis <salieflewis@gmail.com>
2022-10-26 10:09:46 -0400

    gitignore test

--- test/ZorbMinter.t.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
-
-import "forge-std/Test.sol";
-import "../src/ZorbMinter.sol";
-
-contract ZorbMinterTest is Test {
-    function setUp() public {}
-}
