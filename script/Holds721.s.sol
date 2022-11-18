e336f93e1605ddcbbfb37261877d67ec2a986755
Salief Lewis <salieflewis@gmail.com>
2022-11-17 21:53:54 -0500

    remove: old files from "out"

--- script/Holds721.s.sol
+++ script/Holds721.s.sol
@@ -9,11 +9,12 @@ import "solmate/tokens/ERC721.sol";
 contract DeployScript is Script {
     // ===== CONSTRUCTOR INPUTS =====
     ERC721 public ADDRESS_OF_721 = ERC721(0xCa21d4228cDCc68D4e23807E5e370C07577Dd152); // Zorb contract address
+    ERC721 public GOERLI_ADDRESS_OF_721 = ERC721(0x34FE32e6442D14d923953a537B8163365630b5A7); // Test curation pass address
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        new Holds721(ADDRESS_OF_721);
+        new Holds721(GOERLI_ADDRESS_OF_721);
         vm.stopBroadcast();
     }
 }
